fa4bbadb3f6b9fd3335b1f3911a3dbe4f42f4849
arunkapil <arunkapil97@gmail.com>
2020-09-06 18:53:51 +0530

    pre_finalcommingsoon

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var path = require('path')
+
+const app = express();
+app.use('/static',express.static('public'))
+
+app.get('/', function(req,res){
+res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
+
+// ignore
+
